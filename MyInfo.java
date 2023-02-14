deaf9aa17795ff7ef623c35fdb14138be3a529a2
upasana khatiwada <upashanakhatiwada1234@gmail.com>
2023-02-14 16:42:32 +0545

    java first assignment(print output)

---
+++ MyInfo.java
@@ -0,0 +1,7 @@
+public class MyInfo{
+    public static void main(String[] args){
+        System.out.println("Name: Upasana Khatiwada");
+        System.out.println("class: 4th sem BIT");
+        System.out.println("adress: thapathali");
+    }
+}
